d8ed4b39ae08e268536fe7af8c8d94081ec2c2c2
Andrew Ferruolo <andrew.ferruolo@gmail.com>
2022-11-28 11:00:24 -0500

    Made nathan Fixes

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ BG_WIDTH = 1072
 BG_HEIGHT = 504
 IMG_WIDTH = 106
 IMG_HEIGHT = 76
+NUM_ITEMS = 5
+
 
 
 pygame.init()
@@ -20,36 +22,47 @@ screen = pygame.display.set_mode((BG_WIDTH , BG_HEIGHT))
 clock = pygame.time.Clock()            #get a pygame clock object
 item = pygame.image.load('pics/Michigan_Wolverines_Block_M.png').convert()
 background = pygame.image.load('pics/football.png').convert()
+final = GameObject(pygame.Surface((IMG_WIDTH*2, IMG_HEIGHT*2)), 106*2, 20)
 screen.blit(background, (0, 0))
 objects = []
 p = GameObject(item, 106, 20)
 
 
-x_coords = np.random.randint(0, BG_WIDTH - IMG_WIDTH, (10))
-y_coords = np.random.randint(0, BG_HEIGHT - IMG_HEIGHT, (10))
+x_coords = np.random.randint(0, BG_WIDTH - IMG_WIDTH, (NUM_ITEMS))
+y_coords = np.random.randint(0, BG_HEIGHT - IMG_HEIGHT, (NUM_ITEMS))
 idx = 0
-time_records = [time.time()]
 
+iter = 0
 while True:
+
+
+
     if p.is_clicked():
+        print("clicked")
         idx += 1
-        time_records.append(time.time())
-    if idx == 10:
-        print(time_records)
-        sys.exit()
+    # if final.is_clicked():
+    #     print("Memory")
+    #     idx += 1
+    if idx == NUM_ITEMS:
+        print("End iter " + str(iter))
+        iter += 1
+        idx = 0
+        if iter == 5:
+            p = final
+
 
 
     screen.blit(background, (0, 0))
+
     p.set_coords(x_coords[idx], y_coords[idx])
     screen.blit(p.image, p.pos)
+    if iter == 5:
+        screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print(time_records)
+        if event.type == pygame.QUIT or iter == 6:
+            print("Sucess")
             sys.exit()
-
-
-
 
     pygame.display.update()
     clock.tick(60)
